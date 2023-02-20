31af9e75ace43b851c11f4584ca4501bdffec915
Li <1820294178@qq.com>
2023-02-20 18:38:32 +0800

    Initial Commit

---
+++ src/main/java/com/itheima/reggie/service/DishService.java
@@ -0,0 +1,14 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Dish;
+
+public interface DishService extends IService<Dish> {
+    //新增菜品，同时插入菜品对应的口味数据，需要同时操作两张表
+    public void saveWithFlavor(DishDto dishDto);
+
+    public DishDto getByIdWithFlavor(Long id);
+
+    public void updateWithFlavor(DishDto dishDto);
+}
---
+++ src/main/java/com/itheima/reggie/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,72 @@
+package com.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.common.CustomException;
+import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.entity.SetmealDish;
+import com.itheima.reggie.mapper.SetmealMapper;
+import com.itheima.reggie.service.SetmealDishService;
+import com.itheima.reggie.service.SetmealService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
+
+    @Autowired
+    private SetmealDishService setmealDishService;
+    /**
+     * 新增套餐，同时保存套餐和菜品的关联关系
+     *
+     * @param setmealDto
+     */
+    @Transactional
+    public void saveWithDish(SetmealDto setmealDto) {
+        //保存套餐的基本信息
+        this.save(setmealDto);
+
+        Long id = setmealDto.getId();
+        List<SetmealDish> setmealDishes = setmealDto.getSetmealDishes();
+        setmealDishes.stream().map((item) -> {
+            item.setSetmealId(id);
+            return item;
+        }).collect(Collectors.toList());
+
+        //保存套餐和菜品的关联信息
+        setmealDishService.saveBatch(setmealDishes);
+    }
+
+    /**
+     * 删除套餐
+     * @param ids
+     */
+    @Transactional
+    public void removeWithDish(List<Long> ids) {
+        //查询套餐状态，确定可以删除
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.in(Setmeal::getId, ids);
+        queryWrapper.eq(Setmeal::getStatus, 1);
+
+        int count = this.count(queryWrapper);
+
+        //如果不能删除，抛出一个业务异常
+        if (count > 0) {
+            throw new CustomException("套餐正在售卖中，不能删除");
+        }
+
+        //如果可以删除，先删除套餐中的数据
+        this.removeByIds(ids);
+
+        //删除关系表中的数据
+        LambdaQueryWrapper<SetmealDish> dishLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        dishLambdaQueryWrapper.in(SetmealDish::getSetmealId, ids);
+
+        setmealDishService.remove(dishLambdaQueryWrapper);
+    }
+}
